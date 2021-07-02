796f3a37a43873186c84ea90c6caa970068b8390
saloumeh-67 <s.saaraabi@gmail.com>
2021-07-02 20:18:04 +0200

    javascript/javascript1/week1/Houseypricey.js

---
+++ javascript/javascript1/week1/Houseypricey.js
@@ -0,0 +1,13 @@
+var houseWidth = 8; 
+var houseDepth = 10; 
+var houseHeight = 10;
+var gardenSize = 100;
+var houseCost = 2.500.000;
+var volumeInMeters = houseWidth * houseDepth * houseHeight;
+housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+if(housePrice > houseCost) {
+    peter did paye too much 
+}
+else (housePrice < houseCost) {
+    peter did paye fair 
+}
