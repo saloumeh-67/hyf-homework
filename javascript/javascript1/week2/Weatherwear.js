d3c452093889a04f26b5bfe89f1b00129a7cf28a
saloumeh-67 <s.saaraabi@gmail.com>
2021-07-10 19:30:31 +0200

    javascript/javascript1/week2/Weatherwear.js

---
+++ javascript/javascript1/week2/Weatherwear.js
@@ -0,0 +1,8 @@
+function WeatherDressCode(temperature) {
+    if (tempearature < 18) {
+        console.log("wear jacket");
+    }
+    if (tempearature > 18) {
+        console.log("wear shorts and shirt");
+    }
+}
