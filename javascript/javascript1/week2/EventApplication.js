037a01d6a7aaaa2cdcc7dd64c062d87218e0e436
saloumeh <77116575+saloumeh-67@users.noreply.github.com>
2021-07-25 12:53:06 +0200

    Delete EventApplication.js
    delete

--- javascript/javascript1/week2/EventApplication.js
+++
@@ -1,9 +0,0 @@
-const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-function getEventWeekday(daysLeft) {
-const today = new Date();
-const dayIndex = (today.getDay() + daysLeft)% 7;
-console.log ("The event will be held on "  + weekDays[dayIndex] +  "  ");    
-}
-
-console.log(getEventWeekday(9)); // Logs out "Monday"
-console.log(getEventWeekday(3)); // Logs out "Tuesday"
