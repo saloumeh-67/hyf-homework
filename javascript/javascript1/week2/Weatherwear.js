9c20cd8e6394715e86eb63efb62b131637165985
saloumeh <77116575+saloumeh-67@users.noreply.github.com>
2021-07-25 12:53:45 +0200

    Delete Weatherwear.js
    delete

--- javascript/javascript1/week2/Weatherwear.js
+++
@@ -1,9 +0,0 @@
- function WeatherDressCode(tempearature){
-    if (tempearature < 18) {
-        console.log("wear jacket");
-    }
-    else if (tempearature > 18) {
-        console.log("wear shorts and shirt");
-    }
-  
-}
