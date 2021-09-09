56577784f4e878e72ca6fb983cd5d46c3c7cbe86
saloumeh-67 <s.saaraabi@gmail.com>
2021-09-09 17:45:52 +0200

    javascript/javascript2/week1/FindDanishLetters.js

--- javascript/javascript2/week1/FindDanishLetters.js
+++ javascript/javascript2/week1/FindDanishLetters.js
@@ -2,11 +2,17 @@ const danishString = "Jeg har en blå bil";
 const danishString2 = "Blå grød med røde bær";
 function count (string) {  
     let count = {};
-    danishString.split('').forEach(function(string) {
+    string.split('').forEach(function(string) {
        count[string] ? count[string]++ : count[string] = 1;
     });
-    return count;
+    let result = {}
+    if (count["ø"]) {
+      result["ø"] = count["ø"]
+    }
+    result.total=count["ø"];
+    return result;
   }
+  
   console.log(count(danishString));
   console.log(count(danishString2));
 
