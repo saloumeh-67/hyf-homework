26ba7ebc0fa6ee3320d43a1adc6b75c2464e2514
saloumeh-67 <s.saaraabi@gmail.com>
2021-10-17 14:04:26 +0200

    nodejs/week1/homework/warmup.js

--- nodejs/week1/homework/src/backend/app.js
+++ nodejs/week1/homework/src/backend/app.js
@@ -3,10 +3,30 @@ const app = express();
 
 // import data here
 const meals = require("./data/meals");
+const reviews = require("./data/reviews");
+const reservations = require("./data/reservations");
 
 // this is where you will be adding your routes
 app.get("/", async (request, response) => {
-  response.send("asd");
+  res.send("asd");
+});
+
+// Response all meals and includes review it matching each meal
+const renderingMeals = meals.map((item) => {
+  item.reviews = reviews.filter((obj) => obj.mealId === item.id);
+  return item;
+});
+app.get("/meals", function (req, res) {
+  res.send(renderingMeals);
+});
+// Respond cheap meal with + reviews
+const cheapMeals = meals.filter((item) => {
+  item.reviews = reviews.filter((obj) => obj.mealId === item.id);
+  return item.price < 70;
+});
+app.get("/cheap-meals", function (req, res) {
+  res.send(cheapMeals);
 });
 
 module.exports = app;
+app.listen(8080);
