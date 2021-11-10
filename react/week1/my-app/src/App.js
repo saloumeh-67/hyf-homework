a3ab63d8b3ffb3ebf79553aa3239ddf22c12e5b9
saloumeh-67 <s.saaraabi@gmail.com>
2021-11-10 15:44:10 +0100

    new commits

--- react/week1/my-app/src/App.js
+++ react/week1/my-app/src/App.js
@@ -1,32 +1,25 @@
-import react, {useState} from 'react';
-import ToDoList from './ToDoList'
+import React, { useState } from "react";
+import TodoList from "./TodoList";
+
 function App() {
+  const todoList = [
+    {
+      Description: "Get out of bed",
+      Deadline: "Wed Sep 13 2017",
+    },
+    {
+      Description: "Brush teeth",
+      Deadline: "Thu Sep 14 2017",
+    },
+    {
+      Description: "Eat breakfast",
+      Deadline: "Fri Sep 15 2017",
+    },
+  ];
   return (
-    < >
-    const [toDos,setToDos]= useState([])
-    <ToDoList todos ={todos}/> 
-    <input type = "text" >
-      <button>Add Todo</button>
-      <button>Clear Complete Todo's</button>
-      <dive>0 Left To Do</dive>
-  </>
-  )
+    <>
+      <TodoList listItems={todoList} />
+    </>
+  );
 }
-
-      {/* let todoList = [
-        {
-            "Description": "Get out of bed",
-            "Deadline": "Wed Sep 13 2017"
-        },
-        {
-            "Description" : "Brush teeth",
-            "Deadline": "Thu Sep 14 2017"
-        },
-        {
-            "Description": "Eat breakfast",
-            "Deadline": "Fri Sep 15 2017"
-        }
-    ];  */}
-
- 
 export default App;
